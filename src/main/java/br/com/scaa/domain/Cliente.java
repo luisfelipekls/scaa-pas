1918d817091cd60b6b2cda490cff5ec527659f88
Lucas <lucasmarthins1@gmail.com>
2024-10-16 20:53:39 -0300

    Adicionando entidade Cliente

--- src/main/java/br/com/scaa/domain/Cliente.java
+++ src/main/java/br/com/scaa/domain/Cliente.java
@@ -2,28 +2,20 @@ package br.com.scaa.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-public class Assinatura {
+public class Cliente {
     @Id
     private int id;
     private Long codigo;
-    private Date inicioVigencia;
-    private Date fimVigencia;
-    @ManyToOne
-    private Aplicativo aplicativo;
-    @ManyToOne
-    private Cliente cliente;
+    private String nome;
+    private String email;
 }
