485a65bf05b1b6ab307e91b06e0bc0581418de9b
Lucas <lucasmarthins1@gmail.com>
2024-10-16 20:48:59 -0300

    Adicionando entidade Assinatura

---
+++ src/main/java/br/com/scaa/domain/Assinatura.java
@@ -0,0 +1,23 @@
+package br.com.scaa.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Assinatura {
+    @Id
+    private int id;
+    private Long codigo;
+    private Date inicioVigencia;
+    private Date fimVigencia;
+}
