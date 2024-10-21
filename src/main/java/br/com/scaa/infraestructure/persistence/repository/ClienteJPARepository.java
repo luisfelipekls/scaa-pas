8939ce53efda5ffc1e3fde195600b18a300a3875
Lucas <lucasmarthins1@gmail.com>
2024-10-21 20:53:54 -0300

    Adicionando camadas de service e controller.

---
+++ src/main/java/br/com/scaa/infraestructure/persistence/repository/ClienteJPARepository.java
@@ -0,0 +1,4 @@
+package br.com.scaa.infraestructure.persistence.repository;
+
+public class ClienteJPARepository {
+}
---
+++ src/main/java/br/com/scaa/infraestructure/web/ClienteController.java
@@ -0,0 +1,4 @@
+package br.com.scaa.infraestructure.web;
+
+public class ClienteController {
+}
