40bfe7ac07fb37af2fccf4902e0b8c168b8b294c
igor.xavier <i.marcel@edu.pucrs.br>
2024-11-23 19:06:23 -0300

    remove assCache

--- src/main/java/br/com/scaa/asscache/repository/AssinaturaCacheRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.scaa.asscache.repository;
-
-import br.com.scaa.asscache.model.AssinaturaCacheModel;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssinaturaCacheRepository extends CrudRepository<AssinaturaCacheModel, Long> {
-}
